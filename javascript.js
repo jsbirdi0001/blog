96a715e79ec203ec64be3d1cb78f3251bc0f38f8
Jatinder Singh <jsbirdi0001@gmail.com>
2018-09-30 22:20:16 -0400

    Jatinder Singh

--- javascript.js
+++ javascript.js
@@ -102,3 +102,18 @@ $("#blog img").mouseleave(function(){
 	});
 
 })
+
+
+// create the loading sign
+document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'interactive') {
+       document.getElementById('contents').style.visibility="hidden";
+  } else if (state == 'complete') {
+      setTimeout(function(){
+         document.getElementById('interactive');
+         document.getElementById('load').style.visibility="hidden";
+         document.getElementById('contents').style.visibility="visible";
+      },1000);
+  }
+}
